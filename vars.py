fd1cc8cb31050236996ceccec4b50d56491c1c88
Kaizen Castanos <kaizenc98@gmail.com>
2017-12-03 14:54:18 -0500

    vars

---
+++ vars.py
@@ -0,0 +1,7 @@
+apiKey = '3a64a806ffb3ed0a94f771b9ae41a852'
+customerId = '5a0677d5b390353c953a251f'
+checking_id = '5a0678e5b390353c953a2521' #tehcnically checking
+savings_id = '5a06798bb390353c953a2524'
+uber = '5a06b6d2b390353c953a258b'
+chipotle = '5a06b8f5b390353c953a2594'
+zara = '5a06b938b390353c953a2596'
